7c332c7201f7fe253197c804a89cb9b4d3851fb2
Surya Pratap Singh <spsingh@CIANS.COM>
2018-11-30 19:27:33 +0530

    RPS rules

---
+++ RPS/main.py
@@ -0,0 +1,16 @@
+print('welcome to Rock Paper Scissors.'.center(110," "))
+print("Created By Surya Pratap Singh")
+print("Version 1.0")
+username = input("Please Enter Your Name : ")
+if not username.strip():
+	username = "Player"
+computer = "Computer"
+
+#Rules 
+# Rock breaks Scissors
+# Scissors cuts Paper
+# Paper covers Rock.”
+# Defaut lives 5
+# exit with e, exit, q, quit
+# allow small and cap words
+
